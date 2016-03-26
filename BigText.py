ba597f7630c8d7b2d2f5bc3cfaf73e5747084cdd
Gavin <wingman639@gmail.com>
2016-03-26 09:32:45 +0800

    add search feature

--- BigText.py
+++ BigText.py
@@ -4,17 +4,25 @@ PAGE_SIZE = 10000
 
 class TextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        filePath, start = self.getParameter()
+        filePath, start, searchKey = self.getParameters()
         text = self.loadTextFromFile(filePath)
         self.insertText(edit, '\n\n')
+        if searchKey:
+            i = text.find(searchKey, start)
+            if i > 0:
+                start = i
         self.insertText(edit, text[start:min(len(text), PAGE_SIZE)])
 
-    def getParameter(self):
+    def getParameters(self):
         filePath = self.view.substr(self.view.line(0))
-        startStr = self.view.substr(self.view.line(len(filePath) + 1))
+        i = len(filePath) + 1
         print(filePath)
+        startStr = self.view.substr(self.view.line(i))
         print(startStr)
-        return filePath, int(startStr)
+        i += len(startStr) + 1
+        searchKey = self.view.substr(self.view.line(i))
+        print(searchKey)
+        return filePath, int(startStr), searchKey
 
 
     def insertText(self, edit, text):
--- README.md
+++ README.md
@@ -11,6 +11,8 @@ page size: 10000 char
 How to use it:
 1. Input file path in 1st line.
 2. Input start point in 2nd line.
+3. Input search keyword in 3rd line.
 3. Hot key cmd+alt+T to run plugin.
 
+Keyword will be searched in text, and renew start point to 1st found point.
 One page of text from start point of the file will show in SublimeText editer view.
