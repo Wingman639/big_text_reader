177e9828778983f9d3f3fd790a5c9226419992df
Gavin <wingman639@gmail.com>
2016-03-27 23:32:46 +0800

    1. next page, 2. next search key matching result

--- BigText.py
+++ BigText.py
@@ -1,29 +1,65 @@
 import sublime, sublime_plugin
 
-PAGE_SIZE = 10000
 SHOW_AHEAD_SIZE = 100
 
 class TextCommand(sublime_plugin.TextCommand):
+    parameterBegin = None
+    parameterEnd = None
     def run(self, edit):
-        filePath, start, searchKey = self.getParameters()
+        parameters = self.getParameters()
+        filePath = parameters['path']
+        needContinue = parameters['needContinue']
+        appendingBegin = parameters['appendingBegin']
+        appendingEnd = parameters['appendingEnd']
+
         text = self.loadTextFromFile(filePath)
-        self.insertText(edit, '\n\n')
-        if searchKey:
-            i = text.find(searchKey, start)
-            start = self.getStartPointSeveralLinesAhead(text, start, i)
-        end = min(len(text), start + PAGE_SIZE)
-        self.insertText(edit, text[start:end])
+
+        if parameters['needContinue']:
+            start = self.getStartPointWithContinue(parameters, text)
+        else:
+            start = self.getStartPoint(parameters, text)
+        end = min(len(text), start + parameters['pageSize'])
+
+        #self.view.replace(edit, startRegion, str(start))
+        if parameters['needContinue']:
+            self.updateParameters(edit, parameters, start, end)
+
+        if parameters['searchKey']:
+            self.updateSearchOutput(edit, parameters, text[start:end])
+        else:
+            self.appendText(edit, text[start:end])
+
+    def getStartPoint(self, parameters, text):
+        if parameters['searchKey']:
+            return self.getStartWithSearch(parameters, text)
+        return parameters['start']
+
+    def getStartPointWithContinue(self, parameters, text):
+        if parameters['searchKey']:
+            return self.getStartWithSearch(parameters, text)
+        else:
+            if parameters['appendingEnd']:
+                return parameters['appendingEnd']
+        return parameters['start']
+
+
+    def getStartWithSearch(self, parameters, text):
+        i = text.find(parameters['searchKey'], parameters['searchFrom'])
+        #start = self.getStartPointSeveralLinesAhead(text, parameters['searchFrom'], i)
+        start = i
+        parameters['searchFrom'] = i + 1
+        return start
+
+
 
     def getParameters(self):
-        filePath = self.view.substr(self.view.line(0))
-        i = len(filePath) + 1
-        print(filePath)
-        startStr = self.view.substr(self.view.line(i))
-        print(startStr)
-        i += len(startStr) + 1
-        searchKey = self.view.substr(self.view.line(i))
-        print(searchKey)
-        return filePath, int(startStr), searchKey
+        parametersBeginRegion = self.view.find(r'\{', 0)
+        parametersEndRegion = self.view.find(r'\}', 0)
+        self.parameterBegin = parametersBeginRegion.a
+        self.parameterEnd = parametersEndRegion.b
+        parametersRegion = sublime.Region(self.parameterBegin, self.parameterEnd)
+        parametersStr = self.view.substr(parametersRegion)
+        return eval(parametersStr)
 
     def getStartPointSeveralLinesAhead(self, text, start, keyMatchPoint):
         if keyMatchPoint <= 0:
@@ -35,10 +71,65 @@ class TextCommand(sublime_plugin.TextCommand):
         if i > 0:
             return i
 
+    def updateSearchOutput(self, edit, parameters, text):
+        outputRegion = sublime.Region(len(str(parameters)) + 2, self.view.size())
+        self.view.replace(edit, outputRegion, text)
 
-    def insertText(self, edit, text):
+    def updateParameters(self, edit, parameters, start, end):
+        parameters['appendingBegin'] = start
+        parameters['appendingEnd'] = end
+        self.view.replace(edit,
+                          sublime.Region(self.parameterBegin, self.parameterEnd),
+                          str(parameters) + '\n\n')
+
+    '''
+    def dictStr(self, parameters):
+        ' ''
+        {
+        'path': '/Users/wingman/GitHub/big_text_reader/xml/example_400M.XML',
+        'pageSize': 1000,
+        'start': 0,
+        'needContinue': True,
+        'appendingBegin': None,
+        'appendingEnd': None,
+        'searchKey': None,
+        'searchFrom': None,
+        }
+        ' ''
+        items = ['{', '\n',
+                'path: ', parameters['path'], ',\n',
+                'pageSize: ', str(parameters['pageSize']), ',\n',
+                'start: ', str(parameters['start']), ',\n',
+                'needContinue: ', str(parameters['needContinue']), ',\n',
+                'appendingBegin: ', str(parameters['appendingBegin']), ',\n',
+                'appendingEnd: ', str(parameters['appendingEnd']), ',\n',
+                'searchKey: ', str(parameters['searchKey']), ',\n',
+                'searchFrom: ', str(parameters['searchFrom']), ',\n',
+                '}',
+                ]
+        return ''.join(items)
+    '''
+
+    def insertText(self, edit, position, text):
+        self.view.insert(edit, position, text)
+
+    def appendText(self, edit, text):
         self.view.insert(edit, self.view.size(), text)
 
     def loadTextFromFile(self, filePath):
         with open(filePath, 'r') as f:
             return f.read()
+
+
+
+
+'''
+{'needContinue': True,
+'path': '/Users/wingman/GitHub/big_text_reader/xml/small.XML',
+'searchKey': '',
+'searchFrom': 0,
+'appendingEnd': None,
+'start': 0,
+'pageSize': 10000,
+'appendingBegin': None}
+'''
--- README.md
+++ README.md
@@ -9,10 +9,18 @@ line 2: start point
 page size: 10000 char
 
 How to use it:
-1. Input file path in 1st line.
-2. Input start point in 2nd line.
-3. Input search keyword in 3rd line.
-3. Hot key cmd+alt+T to run plugin.
+1. fill parameters:
+{'needContinue': True,
+'path': '/Users/wingman/GitHub/big_text_reader/xml/small.XML',
+'searchKey': '',
+'searchFrom': 0,
+'appendingEnd': None,
+'start': 0,
+'pageSize': 10000,
+'appendingBegin': None}
+
+2. Cmd+Alt+T
+
 
 Keyword will be searched in text, and renew start point to 1st found point.
 One page of text from start point of the file will show in SublimeText editer view.
